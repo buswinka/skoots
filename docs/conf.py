f6729ecc92f8c127ceeb8fb588ce0afcd038c950
buswinka <buswinka@gmail.com>
2023-05-03 16:06:15 -0400

    trying to get sphinx to recognize modules correctly...

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import os.path
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,6 +17,7 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+sys.path.insert(0, os.path.abspath('../skoots'))
 
 master_doc = 'index'
 
