c5c6a3f738fd0db0f009fa8068aaecedd1a9754e
buswinka <buswinka@gmail.com>
2023-05-03 14:54:35 -0400

    minor changes to make readthedocs happy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "skoots"
-copyright = "2022, Chris Buswinka"
+copyright = "2023, Chris Buswinka"
 author = "Chris Buswinka"
-release = "0.0.1"
+release = "0.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    requires = fh.read()
+
 setuptools.setup(
     description="SKeletOn ObjecT Segmentation (SKOOTS)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    python_requires=">=3.9",
-    install_requires=["torch>=1.12.0", "torchvision>=0.13.0", "bism"],
+    install_requires=requires,
+    python_requires=">=3.10",
     entry_points={
         "console_scripts": [
             "skoots-train = skoots.train.__main__:main",
