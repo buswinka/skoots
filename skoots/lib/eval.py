fix to saving in engine. Added experimental scaling functionality to vec2embed

skoots/lib/eval.py
@@ -155,64 +155,66 @@ def eval(
     #     vectors = vectors[:, ...]
 
     del image  # we don't need the image anymore
-    logging.info(f"Saving intermediate outputs")
 
     # torch.save(vectors, filename_without_extensions + '_vectors.trch')
     # torch.save(skeleton, filename_without_extensions + '_unlabeled_skeletons.trch')
 
-    io.imsave(
-        filename_without_extensions + f"_skoots_vectors.tif",
-        vectors.mul(127)
-        .add(127)
-        .int()
-        .cpu()
-        .numpy()
-        .transpose(3, 1, 2, 0)
-        .astype(np.uint16),
-        compression="zlib",
-    )
-    io.imsave(
-        filename_without_extensions + f"_unlabeled_skeletons.tif",
-        skeleton.mul(255)
-        .squeeze(0)
-        .clamp(0, 255)
-        .int()
-        .cpu()
-        .numpy()
-        .transpose(2, 0, 1)
-        .astype(np.uint8),
-        compression="zlib",
-    )
-
-    # zarr.save_array(
-    #     filename_without_extensions + f"_skoots_vectors.zarr",
-    #     vectors.mul(127).add(127).int().cpu().numpy(),
+    # logging.info(f"Saving vector output")
+    # io.imsave(
+    #     filename_without_extensions + f"_skoots_vectors.tif",
+    #     vectors.mul(127)
+    #     .add(127)
+    #     .int()
+    #     .cpu()
+    #     .numpy()
+    #     .transpose(3, 1, 2, 0)
+    #     .astype(np.uint16),
+    #     compression="zlib",
     # )
     #
-    # zarr.save_array(
-    #     filename_without_extensions + f"_skoots_skeleton_unlabeled.zarr",
-    #     skeleton.cpu().numpy(),
+    # logging.info(f"Saving unlabeledc skeleton output")
+    # io.imsave(
+    #     filename_without_extensions + f"_unlabeled_skeletons.tif",
+    #     skeleton.mul(255)
+    #     .squeeze(0)
+    #     .clamp(0, 255)
+    #     .int()
+    #     .cpu()
+    #     .numpy()
+    #     .transpose(2, 0, 1)
+    #     .astype(np.uint8),
+    #     compression="zlib",
     # )
+
+    zarr.save_array(
+        filename_without_extensions + f"_skoots_vectors.zarr",
+        vectors.mul(127).add(127).int().cpu().numpy(),
+    )
+
+    zarr.save_array(
+        filename_without_extensions + f"_skoots_skeleton_unlabeled.zarr",
+        skeleton.cpu().numpy(),
+    )
 
     logging.info(f"Performing an flood fill on skeletons")
     skeleton: Tensor = efficient_flood_fill(skeleton.to(torch.int16))
 
-    io.imsave(
-        filename_without_extensions + f"_skeletons.tif",
-        skeleton
-        .squeeze(0)
-        .clamp(0, 255)
-        .int()
-        .cpu()
-        .numpy()
-        .transpose(2, 0, 1)
-        .astype(np.uint16),
-        compression="zlib",
-    )
-
-    # zarr.save_array(
-    #     filename_without_extensions + f"_skoots_skeleton.zarr", skeleton.cpu().numpy()
+    # io.imsave(
+    #     filename_without_extensions + f"_skeletons.tif",
+    #     skeleton
+    #     .squeeze(0)
+    #     .clamp(0, 255)
+    #     .int()
+    #     .cpu()
+    #     .numpy()
+    #     .transpose(2, 0, 1)
+    #     .astype(np.uint16),
+    #     compression="zlib",
     # )
+
+    zarr.save_array(
+        filename_without_extensions + f"_skoots_skeleton.zarr", skeleton.cpu().numpy()
+    )
 
     logging.info(f"Saving labeled skeletons")
     # torch.save(skeleton, filename_without_extensions + '_skeletons.trch')
@@ -244,7 +246,7 @@ def eval(
         )
 
         _embed = skoots.lib.vector_to_embedding.vector_to_embedding(
-            scale=vector_scale, vector=_vec, N=5
+            scale=vector_scale, vector=_vec, N=1
         )
         _embed += torch.tensor((x, y, z)).view(
             1, 3, 1, 1, 1

skoots/lib/vector_to_embedding.py
@@ -101,12 +101,17 @@ def _vec2embed3D(scale: Tensor, vector: Tensor, n: int = 1) -> Tensor:
     mesh: List[Tensor] = [m.unsqueeze(0).unsqueeze(0) for m in mesh]
     mesh: Tensor = torch.cat(mesh, dim=1)
 
-    vector = vector.mul(num.view(newshape))
+    scaled_vector = vector.mul(num.view(newshape))
+    mesh = mesh + scaled_vector
 
-    mesh = mesh + vector
-
+    scale = 1.0
     for _ in range(n - 1):  # Only executes if n > 1
         # convert to index.
+
+        scale *= 1.0
+
+        scaled_vector = vector.mul(scale * num.view(newshape))
+
         index = mesh.round()
         b, c, x, y, z = index.shape
         for i, k in enumerate([x, y, z]):
@@ -121,7 +126,7 @@ def _vec2embed3D(scale: Tensor, vector: Tensor, n: int = 1) -> Tensor:
         index = index.clamp(0, x * y * z - 1).long()
 
         for i in range(c):
-            mesh[:, [i], ...] = mesh[:, [i], ...] + vector[:, [i], ...].take(index)
+            mesh[:, [i], ...] = mesh[:, [i], ...] + scaled_vector[:, [i], ...].take(index)
 
     return mesh
 